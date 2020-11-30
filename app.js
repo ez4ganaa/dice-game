7b543fde14a282ce261bcbc9d85c758b388e5cce
gana <ez4gana@example.com>
2020-11-30 15:32:40 +0800

    toglomiin huvsagchdiig uusgesen

---
+++ app.js
@@ -0,0 +1,38 @@
+// Тоглогчийн ээлжийг хадгалах хувьсагч, нэгдүгээр тоглогчийг 0, хоёрдугаар тоглогчийг 1 гэж тэмдэглэе
+var activePlayer = 1;
+
+// тоглогчийн цуглуулсан оноог хадгалах хувьсагч
+var score = [0, 0];
+
+// Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
+var roundScore = 0;
+
+
+//Шооны альталаараа буусныг хадгалах хувьсагч хэрэгтэй , 1 6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө
+var diceNumber = Math.floor( Math.random() * 6) + 1;
+
+// Програм эхлэхэд бэлтгэе
+
+document.getElementById("score-0").textContent = 0 ;
+document.getElementById("score-1").textContent = 0 ;
+document.getElementById("current-0").textContent = 0 ;
+document.getElementById("current-1").textContent = 0 ;
+
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+
+document.querySelector(".btn-roll").addEventListener("click", function() {
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+    diceDom.style.display = "block";
+    diceDom.src = 'dice-' + diceNumber + ".png";
+});
+
+document.querySelector(".btn-new").addEventListener("click", function() {
+    document.getElementById("score-0").textContent = 0 ;
+document.getElementById("score-1").textContent = 0 ;
+document.getElementById("current-0").textContent = 0 ;
+document.getElementById("current-1").textContent = 0 ;
+
+    diceDom.style.display = "none";
+});
+
